0ff3c9cbff890dcc30697c953ce694d0615e7087
vitorlosing1 <vitorluricii@hotmail.com>
2024-09-05 22:51:40 -0300

    header update

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,69 +1,73 @@
 import {
-    Keyboard,
-    TouchableWithoutFeedback,
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    Image
+  Keyboard,
+  TouchableWithoutFeedback,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
 } from "react-native";
 import { style } from "./styles";
 import { themes } from "../../global/themes";
 import Icon from "../../assets/returnIcon.png";
 
-
-
 export default function SignUp() {
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={style.container}>
+        <View style={style.topContent}>
+          <Image source={Icon} resizeMode="contain" />
+          <View style={style.topIten}>
+            <Text style={style.headerText}>Cadastro</Text>
+          </View>
+        </View>
 
-    return (
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            <View style={style.container}>
-                <View style={style.topContent}>
-                    <View style={style.topItenI}><Image source={Icon} resizeMode="contain" /></View>
-                    <View style={style.topIten}><Text style={style.headerText}>Cadastro</Text></View>
-                </View>
-
-                <View style={style.headerContent}>
-                    <Text style={style.infoText}>DADOS PESSOAIS</Text>
-                </View>
-                <View style={style.registerContainer}>
-                    <View style={style.inputContent}>
-                        <View style={style.dataInput}>
-                            <TextInput placeholder="NOME"
-                                style={style.inputStyle}
-                                placeholderTextColor={themes.colors.gray}
-                            ></TextInput>
-                        </View>
-                        <View style={style.dataInput}>
-                            <TextInput placeholder="CRMV"
-                                style={style.inputStyle}
-                                placeholderTextColor={themes.colors.gray}
-                            ></TextInput>
-                        </View>
-                        <View style={style.dataInput}>
-                            <TextInput placeholder="EMAIL"
-                                style={style.inputStyle}
-                                placeholderTextColor={themes.colors.gray}
-                            ></TextInput>
-                        </View>
-                        <View style={style.dataInput}>
-                            <TextInput placeholder="SENHA"
-                                style={style.inputStyle}
-                                placeholderTextColor={themes.colors.gray}
-                            ></TextInput>
-                        </View>
-                        <View style={style.dataInput}>
-                            <TextInput placeholder="CONFIRME A SENHA"
-                                style={style.inputStyle}
-                                placeholderTextColor={themes.colors.gray}
-                            ></TextInput>
-                        </View>
-                    </View>
-                </View>
-                <TouchableOpacity style={style.registerBtn}>
-                    <Text style={style.btnText}>CADASTRAR</Text>
-                </TouchableOpacity>
+        <View style={style.headerContent}>
+          <Text style={style.infoText}>DADOS PESSOAIS</Text>
+        </View>
+        <View style={style.registerContainer}>
+          <View style={style.inputContent}>
+            <View style={style.dataInput}>
+              <TextInput
+                placeholder="NOME"
+                style={style.inputStyle}
+                placeholderTextColor={themes.colors.gray}
+              ></TextInput>
             </View>
-        </TouchableWithoutFeedback>
-    )
+            <View style={style.dataInput}>
+              <TextInput
+                placeholder="CRMV"
+                style={style.inputStyle}
+                placeholderTextColor={themes.colors.gray}
+              ></TextInput>
+            </View>
+            <View style={style.dataInput}>
+              <TextInput
+                placeholder="EMAIL"
+                style={style.inputStyle}
+                placeholderTextColor={themes.colors.gray}
+              ></TextInput>
+            </View>
+            <View style={style.dataInput}>
+              <TextInput
+                placeholder="SENHA"
+                style={style.inputStyle}
+                placeholderTextColor={themes.colors.gray}
+              ></TextInput>
+            </View>
+            <View style={style.dataInput}>
+              <TextInput
+                placeholder="CONFIRME A SENHA"
+                style={style.inputStyle}
+                placeholderTextColor={themes.colors.gray}
+              ></TextInput>
+            </View>
+          </View>
+        </View>
+        <TouchableOpacity style={style.registerBtn}>
+          <Text style={style.btnText}>CADASTRAR</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
--- src/pages/signup/styles.ts
+++ src/pages/signup/styles.ts
@@ -1,80 +1,73 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
 import { themes } from "../../global/themes";
 export const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: themes.colors.background,
-        width: "100%",
-        paddingHorizontal: 12
-    },
-    topContent: {
-        justifyContent: 'center',
-        alignItems: "center",
-        flexDirection: "row",
-        height: "14%",
-        width: "100%"
-    },
-    topIten: {
-        width: "60%",
-        justifyContent: "flex-start",
-        marginLeft: "7%"
-
-    },
-    topItenI: {
-        marginRight: "30%",
-        justifyContent: "flex-start",
-    },
-    headerText: {
-        fontSize: 20,
-        color: themes.colors.darkGreen,
-        fontFamily: themes.fonts.title,
-        fontWeight: "bold"
-    },
-    headerContent: {
-        width: "100%",
-        marginBottom: 5,
-    },
-    infoText: {
-        fontSize: 16,
-        fontFamily: themes.fonts.text,
-        color: themes.colors.gray,
-    },
-    registerContainer: {
-        height: "30%",
-        width: "99%",
-        backgroundColor: themes.colors.white,
-        borderRadius: 12
-    },
-    inputContent: {
-        margin: "4%",
-        justifyContent: "space-between",
-        flex: 1
-    },
-    dataInput: {
-        height: "15%",
-        borderBottomWidth: 1,
-        borderBlockColor: themes.colors.gray,
-        marginTop: 5
-    },
-    inputStyle: {
-        flex: 1,
-        fontSize: 16,
-        fontFamily: themes.fonts.text,
-        fontWeight: "regular"
-    },
-    registerBtn: {
-        height: "6%",
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: themes.colors.orange,
-        borderRadius: 8,
-        marginTop: "4%"
-    },
-    btnText: {
-        fontSize: 16,
-        color: themes.colors.white
-    }
-
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: themes.colors.background,
+    width: "100%",
+    paddingHorizontal: 12,
+  },
+  topContent: {
+    alignItems: "center",
+    flexDirection: "row",
+    height: "14%",
+    width: "100%",
+  },
+  topIten: {
+    position: "absolute",
+    width: "100%",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 20,
+    color: themes.colors.darkGreen,
+    fontFamily: themes.fonts.title,
+  },
+  headerContent: {
+    width: "100%",
+    marginBottom: 5,
+  },
+  infoText: {
+    fontSize: 16,
+    fontFamily: themes.fonts.text,
+    color: themes.colors.gray,
+  },
+  registerContainer: {
+    height: "30%",
+    width: "99%",
+    backgroundColor: themes.colors.white,
+    borderRadius: 12,
+  },
+  inputContent: {
+    margin: "4%",
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  dataInput: {
+    height: "15%",
+    borderBottomWidth: 1,
+    borderBlockColor: themes.colors.gray,
+    marginTop: 5,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 16,
+    fontFamily: themes.fonts.text,
+    fontWeight: "regular",
+  },
+  registerBtn: {
+    height: "6%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: themes.colors.orange,
+    borderRadius: 8,
+    marginTop: "4%",
+  },
+  btnText: {
+    fontSize: 16,
+    color: themes.colors.white,
+    fontFamily: themes.fonts.text,
+  },
+});
