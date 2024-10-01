a0e528a6dd4b755d18452ffd7840f2f1b07b4266
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-09-30 22:16:59 -0300

    criando tela para troca de password

---
+++ src/pages/change-password/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export default function ChangePassword() {
+    return (
+        <View></View>
+    )
+}
---
+++ src/pages/change-password/styles.ts
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native"
+import { themes } from "../../global/themes"
+export const style = StyleSheet.create({
+
+})
